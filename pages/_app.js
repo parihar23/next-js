36d7e9008ff2d77ff0ac5987c30a34fefcfc5acc
ravindradtechnoland <ravindrap@dtechnoland.com>
2025-04-06 17:57:43 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import Link from 'next/link';
 
 export default function App({ Component, pageProps }) {
+  <nav style={{ padding: 10, borderBottom: '1px solid #ccc' }}>
+    <Link href="/">Home</Link> | {' '}
+    <Link href="/about">About</Link> | {' '}
+    <Link href="/contact">Contact</Link>
+  </nav>
   return <Component {...pageProps} />;
 }
