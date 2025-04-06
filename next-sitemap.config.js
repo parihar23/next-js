0e8a37170e27c6189f5b40105250466d852927df
ravindradtechnoland <ravindrap@dtechnoland.com>
2025-04-06 18:14:43 +0530

    site map

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+// next-sitemap.config.js
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'http://localhost:3000', // ← Replace with your actual domain
+    generateRobotsTxt: true, // (optional)
+};
